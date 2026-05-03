5e6ae02f309569be0770fc74365f4202eac1508e
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:33 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// ip-portfolio - IP portfolio management - Track and manage intellectual property assets. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
